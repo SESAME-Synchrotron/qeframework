961e559026ee34ed2fd71c77da752f5dc689678b
Abdalla Al-Dalleh <abdalla.ahmad@sesame.org.jo>
2026-02-23 10:16:55 +0300

    QEWidget.cpp: Minor coding styles.

--- qeframeworkSup/project/widgets/QEWidget/QEWidget.cpp
+++ qeframeworkSup/project/widgets/QEWidget/QEWidget.cpp
@@ -849,10 +849,8 @@ void QEWidget::startGui( const QEActionRequests& request )
       QMainWindow* w = new QMainWindow( this->getQWidget() );
       QEForm* gui = new QEForm( request.getArguments().first(), this->getQWidget() );
       gui->setResizeContents( false );
-      if( gui )
-      {
-         if( gui->readUiFile())
-         {
+      if( gui ) {
+         if( gui->readUiFile()) {
             w->setCentralWidget( gui );
             w->setAttribute( Qt::WA_DeleteOnClose );
             w->resize( QSize(gui->width(), gui->height()) );
